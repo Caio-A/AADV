ab43fded94cf1b3f752b8ca5c7996732cd2f4875
leandroschelb <leandro.schelb@gmail.com>
2021-06-20 20:03:21 -0300

    removido pasta que estava inutil

--- app/src/main.js
+++ app/src/main.js
@@ -4,12 +4,10 @@ import { archive, arrowDown, book, home, user } from 'vue-heroicons/src/icons'
 import vuetify from './plugins/vuetify'
 import router from './router/index.js'
 import App from './App.vue'
-import VueHtmlToPaper from 'vue-html-to-paper';
 
 Vue.config.productionTip = false
 HeroIcon.add([archive, arrowDown, user, home, book, ])
 Vue.use(HeroIcon)
-Vue.use(VueHtmlToPaper);
 
 new Vue({
   vuetify,
